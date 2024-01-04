7259d526d2bf772c9b7bbf930b40ded606b6be9c
HomayraOitizzo <hummuoitizzo@gmail.com>
2024-01-04 23:13:00 +0600

    second commit

--- gpa_calculator.py
+++ gpa_calculator.py
@@ -1,67 +1,44 @@
 print("input bangla marks: ")
 bangla = int(input())
 
-if bangla >= 91 and bangla <100:
-    print("A+")
-elif bangla >=81 and bangla <=90:
-    print("A")
-elif bangla >= 71 and bangla <=80:
-    print("B")
-elif bangla >= 61 and bangla <= 70:
-    print("C")
-elif bangla >= 41 and bangla <=60:
-    print("D")
-else:
-    print("F")
+if bangla>100 or bangla<0:
+    print("Marks have to be between 0 and 100 ")
+    exit()
+
 
 # english marks
 print("input english marks: ")
 english = int(input())
 
-if english >= 91 and english <=100:
-    print("A+")
-elif english >=81 and english <=90:
-    print("A")
-elif english >= 71 and english <=80:
-    print("B")
-elif english >= 61 and english <= 70:
-    print("C")
-elif english >= 41 and english <= 60:
-    print("D")
-else:
-    print("F")
+if english >100 or english <0:
+  print("Marks have to be between 0 and 100")
+  exit()
+
 # science marks
 print("input science marks:")
 science = int(input())
 
-if science >= 91 and science <=100:
-    print("A+")
-elif science >=81 and science <=90:
-    print("A")
-elif science >= 71 and science <=80:
-    print("B")
-elif science >= 61 and science <= 70:
-    print("C")
-elif science >= 41 and science <= 60:
-    print("D")
-else:
-    print("F")
+if science >100 or science <0:
+    print("Marks have to be between 0 and 100")
+    exit()
 # mathmarks
 print("input math marks: ")
 math = int(input())
 
-if math >= 91 and math <=100:
-    print("A+")
-elif math >=81 and math <=90:
-    print("A")
-elif math >= 71 and math <=80:
-    print("B")
-elif math >= 61 and math <= 70:
-    print("C")
-elif math >= 41 and math <= 60:
-    print("D")
-else:
-    print("F")
+if math > 100 or math <0:
+    print("Marks have to be between 0 and 100")
+
 
 average = int((bangla + english + math + science) / 4)
-print("Average:", average)
+if average >=91 and average <= 100:
+    print("Obatined Grade: A+")
+elif average>=81 and average <=90:
+    print("Obtained Grade: A")
+elif average>=71 and average<=80:
+    print("Obtained GradeB")
+elif average>=61 and average<=70:
+    print("Obtained Grade: C")
+elif average>=41 and average<=60:
+    print("Obtained Grade: D")
+else:
+    print("obtained Grade: F")
